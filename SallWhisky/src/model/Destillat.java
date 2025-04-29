0ebdab6c29a22e78ef78b9a43be829256678fb1b
nillerdata <eaa24nlgl@students.eaaa.dk>
2025-04-29 13:17:59 +0200

    Destillat og enum kornsort

--- SallWhisky/src/model/Destillat.java
+++ SallWhisky/src/model/Destillat.java
@@ -1,4 +1,58 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Destillat {
+    private int maltbatch;
+    private LocalDate startDato;
+    private LocalDate slutDato;
+    private KornSort kornSort;
+    private double alkoholProcent;
+    private String kommentar;
+    private double mængdeVæske;
+
+    public Destillat(int maltbatch, LocalDate startDato, LocalDate slutDato, KornSort kornSort, double alkoholProcent, String kommentar, double mængdeVæske) {
+        this.maltbatch = maltbatch;
+        this.startDato = startDato;
+        this.slutDato = slutDato;
+        this.kornSort = kornSort;
+        this.alkoholProcent = alkoholProcent;
+        this.kommentar = kommentar;
+        this.mængdeVæske = mængdeVæske;
+    }
+    //GET METODER
+    public int getMaltbatch() {
+        return maltbatch;
+    }
+
+    public LocalDate getStartDato() {
+        return startDato;
+    }
+
+    public LocalDate getSlutDato() {
+        return slutDato;
+    }
+
+    public KornSort getKornSort() {
+        return kornSort;
+    }
+
+    public double getAlkoholProcent() {
+        return alkoholProcent;
+    }
+
+    public String getKommentar() {
+        return kommentar;
+    }
+
+    public double getMængdeVæske() {
+        return mængdeVæske;
+    }
+    //--------------------------------------------------
+    //SET METODER
+    //TODO
+
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
 }
---
+++ SallWhisky/src/model/KornSort.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum KornSort {
+    BYG,RUG,HVEDE,MAJS
+}
